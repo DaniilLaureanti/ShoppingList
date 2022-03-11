7786483d8189b68882e5c8a3149c08ae7cd0ef46
Daniil Laureanti <daniil.laureanti@gmail.com>
2022-03-11 14:01:50 +0200

    added recycle view and will fix a bug when changing the properties of the view when reusing the view

--- app/src/main/java/com/demo/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/demo/shoppinglist/presentation/MainActivity.kt
@@ -4,17 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.demo.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: ShopListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpRecyclerView()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this){
-            Log.d("MainActivityTest", it.toString())
+            adapter.shopList = it
         }
+    }
+
+    private fun setUpRecyclerView(){
+        val rwShopList = findViewById<RecyclerView>(R.id.rw_shop_list)
+        adapter = ShopListAdapter()
+        rwShopList.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/demo/shoppinglist/presentation/ShopListAdapter.kt
@@ -0,0 +1,66 @@
+package com.demo.shoppinglist.presentation
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.demo.shoppinglist.R
+import com.demo.shoppinglist.domain.ShopItem
+
+class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+
+    var shopList = listOf<ShopItem>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    class ShopItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvName = view.findViewById<TextView>(R.id.tv_name)
+        val tvCount = view.findViewById<TextView>(R.id.tv_count)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_shop_disabled,
+            parent,
+            false
+        )
+        return ShopItemViewHolder(view)
+    }
+
+    @SuppressLint("ResourceType")
+    override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
+        val shopItem = shopList[position]
+        val status = if (shopItem.enabled) {
+            "Active"
+        } else {
+            "Not active"
+        }
+
+        holder.itemView.setOnLongClickListener {
+            true
+        }
+
+        if (shopItem.enabled) {
+            holder.tvName.text = "${shopItem.name} $status"
+            holder.tvCount.text = shopItem.count.toString()
+            holder.tvName.setTextColor(R.color.purple_200)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return shopList.size
+    }
+
+    @SuppressLint("ResourceType")
+    override fun onViewRecycled(holder: ShopItemViewHolder) {
+        super.onViewRecycled(holder)
+        holder.tvName.text = ""
+        holder.tvCount.text = ""
+        holder.tvName.setTextColor(R.color.white)
+    }
+}
