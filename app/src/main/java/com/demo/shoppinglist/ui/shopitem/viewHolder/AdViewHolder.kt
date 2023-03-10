bf7eaa4933aa2ea06ff50b56fd552ffe06eb5b49
Daniil Laureanti <daniil.laureanti@gmail.com>
2023-03-10 19:30:37 +0100

    Cleaning the project

--- app/src/main/java/com/demo/shoppinglist/ui/shopitem/viewHolder/AdViewHolder.kt
+++ app/src/main/java/com/demo/shoppinglist/ui/shopitem/viewHolder/AdViewHolder.kt
@@ -1,12 +1,7 @@
 package com.demo.shoppinglist.ui
 
 import androidx.databinding.ViewDataBinding
-import com.demo.shoppinglist.R
 import com.demo.shoppinglist.ui.shopitem.viewHolder.ItemListViewHolder
-import com.google.android.gms.ads.AdListener
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdView
-import com.google.android.gms.ads.LoadAdError
 
 class AdViewHolder(override val binding: ViewDataBinding)
     : ItemListViewHolder(binding)
